508b8922e1fe711755443eef177e42d3ca5037e4
Pavel Khokhlov <gg.pavel.kh@gmail.com>
2026-01-03 20:51:36 +0300

    bug: fix fonts link astro link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,21 @@
 import { defineConfig } from "astro/config";
 // @ts-ignore
 import vue from "@astrojs/vue";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
+  site: 'https://pavel-khokhlov.github.io',
+  output: "server",
+  adapter: node({
+    mode: 'standalone',
+  }),
+  build: {
+    format: "directory"
+  },
+  server: {
+    host: true,
+  },
+  trailingSlash: "ignore",
 });
